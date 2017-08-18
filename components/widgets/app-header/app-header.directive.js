bb81abbb36b4764e33dba96528fbc6da08fa1688
brayan1488@hotmail.com <brayan1488@hotmail.com>
2017-08-17 23:23:09 -0500

    cambios menu hallearn

--- components/widgets/app-header/app-header.directive.js
+++ components/widgets/app-header/app-header.directive.js
@@ -3,8 +3,8 @@
 
     angular.module('app.header', []).directive('appHeader', appHeader);
 
-    appHeader.$inject = ['$state' ];
-   
+    appHeader.$inject = ['$state'];
+
 
     function appHeader($state) {
 
@@ -20,9 +20,6 @@
             templateUrl: 'components/widgets/app-header/app-header.template.html'
         };
 
-
-
-
         function AppHeaderController($scope, $window, $state, $uibModal, appHeaderFactory) {
 
             var self = this;
@@ -30,104 +27,83 @@
             self.showConfig = false;
             self.showTemplate = false;
             self.showAssistant = false;
-            self.showMonitor = false;  
+            self.showMonitor = false;
 
             self.showConfig1 = false;
             self.showTemplate1 = false;
             self.showAssistant1 = false;
-            self.showMonitor1 = false;  
+            self.showMonitor1 = false;
 
             self.posicion = 1;
 
             var currentProfile = $window.localStorage.perfilid;
 
-        
-
             self.logout = logout;
-         
-            
-            var usuario = JSON.parse($window.localStorage.usuario) ; 
+
+            var usuario = JSON.parse($window.localStorage.usuario);
             self.currentUser = usuario.username;
             self.currentAdministrador = usuario.administrador;
             self.currentProfesor = usuario.profesor;
             self.currentAlumno = usuario.alumno;
             self.changePassword = changePassword;
-            
-            console.log(self.currentAdministrador);
 
+            activate();
 
-activate();
-        
-function activate()
-{
-    if(self.currentAdministrador == true)
-    {
-         self.showAdministrador = true;
-    }
-    if(self.currentProfesor == true)
-    {
-          self.showProfesor = true;
-    }
-    if(self.currentAlumno==true) 
-    {
-         self.showAlumno = true;
-    }
-}
-       
- menu();
- submenu_alumno();
- submenu_profesor();
- submenu_admin();
+            function activate() {
+                if (self.currentAdministrador == true) {
+                    self.showAdministrador = true;
+                }
+                if (self.currentProfesor == true) {
+                    self.showProfesor = true;
+                }
+                if (self.currentAlumno == true) {
+                    self.showAlumno = true;
+                }
+            }
 
+            menu();
+            submenu_alumno();
+            submenu_profesor();
+            submenu_admin();
 
- 
+            //funcion para que aparesca o desaparesca el menu 
+            function menu() {
+                var contador = 1;
+                $('.open_list').click(function () {
+                    if (contador == 1) {
+                        $('nav').animate({
+                            left: '0'
+                        });
+                     //   document.getElementsByClassName('container-fluid').style.paddingLeft = "195px";
+                        contador = 0;
+                    } else {
+                        contador = 1;
+                        $('nav').animate({
+                            left: '-100%'
+                        });
+                    }
+                });
+            };
 
-//funcion para que aparesca o desaparesca el menu 
-function menu(){
-    var contador = 1;
-	$('.open_list').click(function(){
-		if(contador == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		} 
-	});
-};
+            function submenu_alumno() {
+                $('.submenu_alumno').click(function () {
+                    $(this).children('.children_alumno').slideToggle();
+                });
+            }
 
+            function submenu_profesor() {
+                $('.submenu_profesor').click(function () {
+                    $(this).children('.children_profesor').slideToggle();
+                });
+            }
 
-function submenu_alumno()
-{
-    $('.submenu_alumno').click(function(){
-         $(this).children('.children_alumno').slideToggle();
-    });
-}
+            function submenu_admin() {
+                $('.submenu_admin').click(function () {
+                    $(this).children('.children_admin').slideToggle();
+                });
+            }
 
-
-function submenu_profesor()
-{
-    $('.submenu_profesor').click(function(){
-         $(this).children('.children_profesor').slideToggle();
-    });
-}
-
-
-function submenu_admin()
-{
-    $('.submenu_admin').click(function(){
-         $(this).children('.children_admin').slideToggle();
-    });
-}
-
-
-
-        function logout() {
-
+            function logout() {
                 $state.go('login');
             };
 
@@ -158,15 +134,13 @@ function submenu_admin()
         };
     }
 
-    function ModalController(appHeaderFactory,$window,$uibModalInstance) {
+    function ModalController(appHeaderFactory, $window, $uibModalInstance) {
         var self = this;
 
         self.nameModule = 'Cambiar Contraseña';
         self.confirmChange = confirmChange;
         self.cancel = cancel;
-        //$scope.nameModule = 'Cambiar Contraseña';
-        //console.log('entro modal');
-
+    
         function confirmChange() {
             var changes = {
                 usuarioid: $window.localStorage.usuarioid,
@@ -177,39 +151,33 @@ function submenu_admin()
 
             appHeaderFactory.updatePassword($window.localStorage.usuarioid, changes).then(function (response) {
                 $uibModalInstance.close('');
-            },function(reason){
+            }, function (reason) {
                 //console.log(reason);
                 self.error = reason.data;
             });
         }
 
-        function cancel()
-        {
-           $uibModalInstance.close(''); 
+        function cancel() {
+            $uibModalInstance.close();
         }
 
     };
 
 
-
-
-
-
-//Modal logout 
+    //Modal logout 
 
     self.showToast2 = showToast2;
 
-    function showToast2()
-     {
-                  var toast = $mdToast.simple()
-                     .textContent('Hello World!')
-                     .action('OK')
-                     .highlightAction(false);                     
-                  $mdToast.show(toast).then(function(response) {
-                     if ( response == 'ok' ) {
-                        alert('You clicked \'OK\'.');
-                     }
-                  });			   
+    function showToast2() {
+        var toast = $mdToast.simple()
+            .textContent('Hello World!')
+            .action('OK')
+            .highlightAction(false);
+        $mdToast.show(toast).then(function (response) {
+            if (response == 'ok') {
+                alert('You clicked \'OK\'.');
+            }
+        });
     }
 
-} ());
+}());
