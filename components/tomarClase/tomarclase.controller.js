f79d4e84b56a3b2c99c8692054cb60ee1ece5a8f
brayan1488@hotmail.com <brayan1488@hotmail.com>
2017-08-25 21:18:47 -0500

    implementacion de video en hallearn

--- components/tomarClase/tomarclase.controller.js
+++ components/tomarClase/tomarclase.controller.js
@@ -5,20 +5,20 @@
         .module('app.tomarclase')
         .controller('tomarclaseController', tomarclaseController);
 
-    tomarclaseController.$inject = ['$stateParams', '$state', '$websocket', 'PTemaFactory', '$window', '$scope'];
+    tomarclaseController.$inject = ['$stateParams', '$state', '$websocket', 'PTemaFactory', '$window', '$scope', '$timeout'];
 
-    function tomarclaseController($stateParams, $state, $websocket, PTemaFactory, $window, $scope) {
+    function tomarclaseController($stateParams, $state, $websocket, PTemaFactory, $window, $scope, $timeout) {
 
 
         var self = this;
-        var socket = window.io('http://192.168.1.5:3002/');
+        var socket = window.io('http://192.168.1.6:3002/');
         self.newMessage = undefined;
         self.messages = [];
 
-        var datauser = JSON.parse($window.localStorage.usuario) ;
+        var datauser = JSON.parse($window.localStorage.usuario);
         var usuario = datauser.username;
         console.log("usuario", usuario);
-                                   
+
         //Metodo que envia el mensaje al socket
         self.sendMessage = function () {
             var newMessage = {
@@ -37,5 +37,64 @@
                 self.messages.push(msg);
             });
         });
+
+
+
+
+        //metodos para el video
+
+        // var canvas = document.getElementById("preview");
+        //var canvas = angular.element('#preview');-
+        var canvas = null;
+
+        $timeout(function () {
+              canvas = document.getElementById("preview_video");
+
+
+        console.log("Canvas", canvas);
+
+        var context = canvas.getContext("2d");
+
+
+        canvas.width = 900;
+        canvas.height = 700;
+
+        context.width = canvas.width;
+        context.height = canvas.height;
+
+        var video = document.getElementById("video");
+
+        function loadCam(stream) {
+            video.src = window.URL.createObjectURL(stream);
+            console.log("se cargo correctamente");
+        }
+
+        function loadFail() {
+            console.log("ocurrio un error para cargar la camara");
+        }
+
+        function viewVideo(video, context) {
+            context.drawImage(video, 0, 0, context.width, context.height);
+            socket.emit("stream-video", canvas.toDataURL('image/webp'));
+        }
+
+
+        //enciende el servicio de camara del pc emisor
+        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia
+            || navigator.mozGetUserMedia || navigator.msgGetUserMedia)
+
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({ video: true }, loadCam, loadFail);
+        }
+
+        setInterval(function () {
+            //console.log("esta copiando a canvas");
+            viewVideo(video, context);
+        }, 30);
+
+        }, 1000);
+      
+
+
     }
 }(window, window.angular));
