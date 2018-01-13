f2a325938b6c2f17fce3fd6ca9b1050b349ffedf
brayan1488@hotmail.com <brayan1488@hotmail.com>
2018-01-13 16:44:27 -0500

    Organizacion de estructura del front

--- components/app.libraries.js
+++ components/app.libraries.js
@@ -4,17 +4,15 @@
     // Inyecta las librerias de las directivas previamente instalas y que se van a utilizar
     angular.module('app.libraries', [
         'ui.router',
-        'ngMessages',
         'ui.bootstrap',
         'ngMaterial',
         'ngAnimate',
         'app.widgets',
         'datatables',
-        'ngSanitize',
         'ui.select',
-        'ngWebSocket',
         'toastr',
-        'pascalprecht.translate'
+        'pascalprecht.translate',
+        'angular-google-analytics'
     ]);
 
 }());
