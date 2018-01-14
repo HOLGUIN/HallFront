d8d18840ee7e26d104b3ff747072f12f85640927
brayan1488@hotmail.com <brayan1488@hotmail.com>
2018-01-13 19:11:02 -0500

    gulp implementation

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename');
+
+gulp.task('uglify', function () {
+    gulp.src(['components/*.js', 'components/**/*.js', 'components/**/**/*.js'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('components/'))
+})
+
+
+
